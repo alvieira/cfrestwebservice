bc8e03e57a0e51c924b6b8b64caf94974d65da89
Aloisio Eduardo Vieira <alvieira@br.ibm.com>
2018-10-09 16:19:43 -0300

    first commit

---
+++ src/main/java/com/example/cfrestwebservice/CfrestwebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.example.cfrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfrestwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfrestwebserviceApplication.class, args);
+	}
+}
